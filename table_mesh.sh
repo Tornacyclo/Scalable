43832ac65d8ae57127fcc23c5371225b43930fe3
User <user@nonnos-ubuntu.>
2024-08-08 14:55:16 +0200

    chore: Update table_mesh.sh script to include epsilon value for UNTANGLE-2D method

--- table_mesh.sh
+++ table_mesh.sh
@@ -19,7 +19,11 @@ for mesh in "${mesh_files[@]}"; do
     for iter in "${iterations[@]}"; do
         for method in "${methods[@]}"; do
             echo "Running project with mesh: $mesh, iterations: $iter, method: $method"
-            ./project/SLIM_intern "$mesh" 1 max_iterations="$iter" "$method"
+            if [ "$method" == "UNTANGLE-2D" ]; then
+                ./project/SLIM_intern "$mesh" 1 max_iterations="$iter" "$method" epsilon=1e-1
+            else
+                ./project/SLIM_intern "$mesh" 1 max_iterations="$iter" "$method"
+            fi
             if [ $? -ne 0 ]; then
                 echo "Run failed with mesh: $mesh, iterations: $iter, method: $method"
             fi
